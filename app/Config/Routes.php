bea0bf151ae16db18cae9c1b89b79bad57c27fe7
naufalmuhammadn <naufalmuhammadmail@gmail.com>
2023-12-12 00:33:05 +0700

    feat: add post handler

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,5 +7,5 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->post('/login', 'User::login');
-$routes->get('/transaction', 'Transaction::index');
+$routes->resource('transaction');
 
--- app/Controllers/Transaction.php
+++ app/Controllers/Transaction.php
@@ -11,4 +11,24 @@ class Transaction extends ResourceController
         $transactions = $this->model->findAll();
         return $this->respond($transactions);
     }
+
+    public function create(){
+        helper(['form']);
+
+        $rules = [
+            'address' => 'required|min_length[6]',
+        ];
+
+        if(!$this->validate($rules)){
+            return $this->fail($this->validator->getErrors());
+        } else{
+            $data = [
+                'transaction_address' => $this->request->getVar('address'),
+                'transaction_delivery_cost' => 10000,
+            ];
+            $transaction_id = $this->model->insert($data);
+            $data['transaction_id'] = $transaction_id;
+            return $this->respondCreated($data);
+        }
+    }
 }
