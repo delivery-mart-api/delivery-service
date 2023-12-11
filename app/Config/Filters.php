88c88533b4b419c3d22f655160585c24a6b08a70
naufalmuhammadn <naufalmuhammadmail@gmail.com>
2023-12-12 00:32:18 +0700

    feat: add auth filter

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -25,6 +25,7 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
+        'oauthfilter'   => \App\Filters\OauthFilter::class
     ];
 
     /**
@@ -67,5 +68,7 @@ class Filters extends BaseConfig
      * Example:
      * 'isLoggedIn' => ['before' => ['account/*', 'profiles/*']]
      */
-    public array $filters = [];
+    public array $filters = [
+        'oauthfilter' => ['before' => ['transaction', 'transaction/*']]
+    ];
 }
