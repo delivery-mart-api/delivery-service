3c261add8dae667450d5ba45431f172ee4fa8980
naufalmuhammadn <naufalmuhammadmail@gmail.com>
2023-12-15 22:30:34 +0700

    feat: update interface

--- app/Views/login_view.php
+++ app/Views/login_view.php
@@ -7,34 +7,43 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container mt-5 col-5">
-        <div class="card">
-            <div class="card-header bg-primary text-white">
-                LOGIN
-            </div>
-            <div class="card-body">
-                <form action="" method="POST">
-                    <?php if(session()->getFlashdata('error')) :?>
-                        <div class="alert alert-danger">
-                            <?php echo session()->getFlashdata('error')?>
+    <div class="item-center justify-content-lg-center max-height: 100%">
+        <div class="text-center">
+            <h1>Belanja Cepat, Belanja HeMart!</h1>
+        </div>
+        <div class="container mt-5 col-5">
+            <div class="card">
+                <div class="card-header bg-primary text-white">
+                    LOGIN
+                </div>
+                <div class="card-body">
+                    <form action="" method="POST">
+                        <?php if(session()->getFlashdata('error')) :?>
+                            <div class="alert alert-danger">
+                                <?php echo session()->getFlashdata('error')?>
+                            </div>
+                        <?php endif;?>
+                        <div class="mb-3">
+                            <label for="inputPhone" class="form-label">
+                                Phone Number
+                            </label>
+                            <input type="text" name="phone" class="form-control" value="<?php echo session()->getFlashdata('phone')?>" id="inputPhone">
                         </div>
-                    <?php endif;?>
-                    <div class="mb-3">
-                        <label for="inputPhone" class="form-label">
-                            Phone Number
-                        </label>
-                        <input type="text" name="phone" class="form-control" value="<?php echo session()->getFlashdata('phone')?>" id="inputPhone">
-                    </div>
-                    <div class="mb-3">
-                        <label for="inputPassword" class="form-label">
-                            Password
-                        </label>
-                        <input type="text" name="password" class="form-control" id="inputPassword">
-                    </div>
-                    <div class="mb-3">
-                        <input type="submit" name="login" class="btn btn-primary" value="Login">
-                    </div>
-                </form>
+                        <div class="mb-3">
+                            <label for="inputPassword" class="form-label">
+                                Password
+                            </label>
+                            <input type="text" name="password" class="form-control" id="inputPassword">
+                        </div>
+                        <div class="mb-3">
+                            <input type="submit" name="login" class="btn btn-primary" value="Login">
+                        </div>
+                    </form>
+                    
+                </div>
+            </div>
+            <div class='my-2'>
+                <a href='/registrasi' class='text-primary fw-5'>Belum punya akun?</a>
             </div>
         </div>
     </div>
--- app/Views/supermarket_view.php
+++ app/Views/supermarket_view.php
@@ -8,19 +8,30 @@
 </head>
 <body>
     <div class="d-flex flex-column">
-        <div class="justify-content-center">
-            Cari supermarket terdekat!
+        <div class="text-center">
+            <h2 class="text-primary">Cari supermarket terdekat!</h2>
         </div>
-        <div class="d-flex flex-col">
+        <div class="d-flex flex-wrap p-3 item-center">
             <!-- Supermarket card -->
-            <div class="card" style="width: 18rem">
-                <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card p-2 mx-2 my-4" style="width: 18rem">
+                <img class="card-img-top" src="https://awsimages.detik.net.id/community/media/visual/2019/11/28/782baf48-5106-4de3-a06a-74d4346a84a9_169.jpeg?w=1200" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Supermarket</h5>
                     <p class="card-text">Jalan Soekarno Hatta</p>
                     <a href="#" class="btn btn-primary">Kunjungi</a>
                 </div>
             </div>
+            <!-- Supermarket card -->
+            <div class="card p-2 mx-2 my-4" style="width: 18rem">
+                <img class="card-img-top" src="https://awsimages.detik.net.id/community/media/visual/2019/11/28/782baf48-5106-4de3-a06a-74d4346a84a9_169.jpeg?w=1200" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Supermarket</h5>
+                    <p class="card-text">Jalan Soekarno Hatta</p>
+                    <a href="#" class="btn btn-primary">Kunjungi</a>
+                </div>
+            </div>
+           
+            
         </div>
     </div>
 </body>
