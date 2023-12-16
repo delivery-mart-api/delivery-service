1659210b7ae67d0ec27eb8941ba53cdb88856cb0
naufalmuhammadn <naufalmuhammadmail@gmail.com>
2023-12-17 00:34:25 +0700

    fix: update model

--- app/Models/TransactionModel.php
+++ app/Models/TransactionModel.php
@@ -5,5 +5,17 @@ use CodeIgniter\Model;
 class TransactionModel extends Model{
     protected $table = 'transaction';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['delivery_cost', 'address', 'created_at', 'user_id'];
+    protected $allowedFields = ['delivery_cost', 'address', 'created_at', 'user_id', 'supermarket_id'];
+
+    public function findByUserId($id)
+    {
+        $data = $this->where('user_id', $id)->findAll();
+
+        if ($data) {
+            return $data;
+        }
+
+        return false;
+    }
 }
+
