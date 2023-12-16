379af17aef211b482e82173e852f05fd2920ce8d
naufalmuhammadn <naufalmuhammadmail@gmail.com>
2023-12-16 18:12:34 +0700

    fix: update routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,12 +5,15 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->post('/login', 'User::login');
-$routes->post('/', 'Home::index');
+$routes->get('/', 'LoginController::index');
+$routes->post('/', 'LoginController::login_user');
+$routes->get('/register', 'Registration::index');
 $routes->post('/register', 'User::register');
 $routes->post('/mitra/register', 'Supermarket::register');
-$routes->post('/mitra/login', 'Supermarket::login');
-$routes->resource('transaction');
-$routes->resource('supermarket');
-
+$routes->get('/mitra/register', 'Registration::registration_supermarket');
+$routes->post('/mitra/login', 'LoginController::login_supermarket');
+$routes->post('/logout', 'LoginController::logout');
+$routes->get('/transaction/(:any)/(:any)', 'Transaction::index/$1/$2');
+$routes->post('/transaction', 'Transaction::create');
+$routes->get('/supermarket', 'Supermarket::index');
+$routes->get('/supermarket/(:any)', 'Supermarket::details/$1');
