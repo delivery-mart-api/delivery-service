7b2e0884b5cf475790beca4efbfb912c46e14468
naufalmuhammadn <naufalmuhammadmail@gmail.com>
2023-12-17 00:31:21 +0700

    feat: add new route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,13 +7,19 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'LoginController::index');
 $routes->post('/', 'LoginController::login_user');
+
 $routes->get('/register', 'Registration::index');
 $routes->post('/register', 'User::register');
+
 $routes->post('/mitra/register', 'Supermarket::register');
 $routes->get('/mitra/register', 'Registration::registration_supermarket');
 $routes->post('/mitra/login', 'LoginController::login_supermarket');
+
 $routes->post('/logout', 'LoginController::logout');
+
 $routes->get('/transaction/(:any)/(:any)', 'Transaction::index/$1/$2');
 $routes->post('/transaction', 'Transaction::create');
+$routes->get('/transaction', 'Transaction::history');
+
 $routes->get('/supermarket', 'Supermarket::index');
 $routes->get('/supermarket/(:any)', 'Supermarket::details/$1');
