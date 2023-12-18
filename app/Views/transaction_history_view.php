413a7f60e16010ef3326d876369f7d12f27340f5
naufalmuhammadn <naufalmuhammadmail@gmail.com>
2023-12-18 14:56:22 +0700

    feat: add new table

--- app/Views/transaction_history_view.php
+++ app/Views/transaction_history_view.php
@@ -6,19 +6,37 @@
             <h2 class="text-primary">Riwayat Transaksi</h2>
         </div>
         <div class="d-flex flex-column p-3 items-center">
-        <?php if (empty($transactions)) : ?>
-            <p class="text-center text-body-secondary fs-5">Belum ada transaksi</p>
-        <?php else : ?>
-            <?php foreach ($transactions as $transaction) : ?>
-                <ul class="list-group list-group-horizontal mx-auto my-2">
-                    <li class="list-group-item"><?= $products[$transaction['product_id']]?></li>
-                    <li class="list-group-item"><?= $transaction['quantity']?></li>
-                    <li class="list-group-item">Rp <?= $transaction['delivery_cost']?></li>
-                    <li class="list-group-item"><?= $transaction['address']?></li>
-                    <li class="list-group-item"><?= $transaction['created_at']?></li>
-                </ul>
-            <?php endforeach; ?>
-        <?php endif; ?>
+            <?php if (empty($transactions)) : ?>
+                <p class="text-center text-body-secondary fs-5">Belum ada transaksi</p>
+            <?php else : ?>
+                <?php
+                usort($transactions, function ($a, $b) {
+                    return strtotime($b['created_at']) - strtotime($a['created_at']);
+                });
+                ?>
+                <table class="table table-bordered mb-3">
+                    <thead>
+                        <tr>
+                            <th>Produk</th>
+                            <th>Kuantitas</th>
+                            <th>Biaya Pengiriman</th>
+                            <th>Alamat Kirim</th>
+                            <th>Created At</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($transactions as $transaction) : ?>
+                            <tr>
+                                <td><?= $products[$transaction['product_id']] ?></td>
+                                <td><?= $transaction['quantity'] ?></td>
+                                <td>Rp <?= $transaction['delivery_cost'] ?></td>
+                                <td><?= $transaction['address'] ?></td>
+                                <td><?= $transaction['created_at'] ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            <?php endif; ?>
         </div>
     </div>
 <?= $this->endSection(); ?>
