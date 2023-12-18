1604d3242677e4da54d3607aaf7375093cbebb53
naufalmuhammadn <naufalmuhammadmail@gmail.com>
2023-12-18 13:36:15 +0700

    fix: add quantity atribiute

--- app/Models/TransactionModel.php
+++ app/Models/TransactionModel.php
@@ -5,7 +5,7 @@ use CodeIgniter\Model;
 class TransactionModel extends Model{
     protected $table = 'transaction';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['delivery_cost', 'address', 'created_at', 'user_id', 'supermarket_id', 'product_id'];
+    protected $allowedFields = ['delivery_cost', 'address', 'created_at', 'user_id', 'supermarket_id', 'product_id', 'quantity'];
 
     public function findByUserId($id)
     {
